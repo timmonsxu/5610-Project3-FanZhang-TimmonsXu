6afae50cb54abc384beaf8ceedbc45296cca27e0
unknown <xu.jiazh@northeastern.edu>
2025-04-23 01:34:15 +0800

    try 1.0

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5001/api";
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5001/api";
 
 const api = axios.create({
   baseURL: API_URL,
@@ -27,6 +27,7 @@ api.interceptors.response.use(
   (response) => response,
   (error) => {
     if (error.response?.status === 401) {
+      localStorage.removeItem("token");
     }
     return Promise.reject(error);
   }
